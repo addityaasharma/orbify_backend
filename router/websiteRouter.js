b70e80cee55199cbd89d1ee8158a404717ee04f2
addityaasharma <adityasharma96655@gmail.com>
2025-07-09 16:30:47 +0530

    changes done

--- router/websiteRouter.js
+++ router/websiteRouter.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { middleware } from '../config/middleware.js';
+import { addWebsite, getWebsite } from '../controller/websiteController.js';
 
 const router = express.Router()
 
-router.post('/website', middleware, adminSignupOTP)
+router.post('/website', middleware, addWebsite)
+router.get('/website', middleware, getWebsite)
 
 export default router;
