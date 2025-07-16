a97fec3bde280a7e704262361c76224ff85f1aa2
addityaasharma <adityasharma96655@gmail.com>
2025-07-16 14:52:44 +0530

    changes done

--- server.js
+++ server.js
@@ -7,32 +7,28 @@ import websiteRouter from './router/websiteRouter.js'
 import userview_router from './router/user_view_router/userview_routes.js'
 import webContent from './router/webContent.js'
 import { dynamicCorsMiddleware } from './config/db.js'
-import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 const PORT = process.env.PORT
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
-app.use(cors(dynamicCorsMiddleware))
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')))
+
+app.use(cors(dynamicCorsMiddleware))
 app.use(express.json())
 
-//routes
 app.use('/admin', authRouter)
 app.use('/', websiteRouter)
 app.use('/content', webContent)
-
-//user_routes
 app.use('/public', userview_router)
 
-//server
 server()
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`App is running on port - http://localhost:${PORT}`)
+  console.log(`App is running on port - http://localhost:${PORT}`)
 })
